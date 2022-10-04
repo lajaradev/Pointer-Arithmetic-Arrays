dd1a454b11097d714b4a3387810d67be51fe3196
Samuel Lajara <lajara.agullo@gmail.com>
2022-10-04 17:50:19 +0200

    V.1.0

---
+++ p0t1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+    FILE *f, *f5, *f10; // f1 (reading), f5 (decimals), f10 (decimals)
+    f = fopen ("data10doubles.bin", "rb");
+    
+    if (f == NULL){ // Check that file is not empty
+        printf("Can't be opened data10doubles.bin");
+        return -1;
+    }
+
+    int bytes_read;
+    double num_doubles [10];
+    
+    bytes_read = fread(&num_doubles, sizeof(double), 10, f);
+    
+    f5 = fopen("exit_5_decimals.txt", "w");
+
+    for(int i = 0; i < 10; i++){
+        printf("%.5f\n", num_doubles[i]);
+        fprintf(f5, "%.5f\n", num_doubles[i]);
+    }
+
+    f10 = fopen("exit_10_decimals.txt", "w");
+
+    for(int i = 0; i < 10; i++){
+        printf("%.10f\n", num_doubles[i]);
+        fprintf(f10, "%.10f\n", num_doubles[i]);
+    }
+
+    fclose(f);
+    return 0;
+}
